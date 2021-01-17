dad4ac3b8df5645cfcae36abbc6601894b2574ab
ValerXenus <valeraxenus@mail.ru>
2021-01-18 01:41:28 +0300

    Initial files commit

---
+++ Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace KazanAirportWebApp.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="KazanAirportWebApp.WebApiApplication" Language="C#" %>
