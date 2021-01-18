622aaef93b14c191f50cd1afa4394f570f54bc66
ValerXenus <valeraxenus@mail.ru>
2021-01-19 00:55:26 +0300

    Footer + new grid styles

--- kazan_airport_client/src/components/NavigationMenu/NavigationMenu.js
+++ kazan_airport_client/src/components/NavigationMenu/NavigationMenu.js
@@ -5,16 +5,21 @@ import styles from './NavigationMenu.module.css';
 
 const NavigationMenu = () => {
     return (
-        <div className={styles.menuStyle}>
-            <img src={logo} style={{height: 40}} alt="logo" />
+        <div className={styles.menuStyle}>            
             <nav>
-                <NavLink to="/schedule" >Табло рейсов</NavLink>
-                <NavLink to="/" >Услуги</NavLink>
-                <NavLink to="/" >Онлайн-регистрация</NavLink>
-                <NavLink to="/" >Купить билет</NavLink>
-                <NavLink to="/" >Как добраться</NavLink>
-                <NavLink to="/" >Схема терминалов</NavLink>
-                <NavLink to="/" >О нас</NavLink>
+                <NavLink to="/">
+                    <img src={logo} style={{height: 60}} alt="logo" />
+                </NavLink>
+                <div style={{float: 'right', marginTop: 20}}>
+                    <NavLink to="/schedule" >Табло рейсов</NavLink>
+                    <NavLink to="/" >Услуги</NavLink>
+                    <NavLink to="/" >Онлайн-регистрация</NavLink>
+                    <NavLink to="/" >Купить билет</NavLink>
+                    <NavLink to="/" >Как добраться</NavLink>
+                    <NavLink to="/" >Схема терминалов</NavLink>
+                    <NavLink to="/" >О нас</NavLink>
+                    <NavLink to="/">Войти</NavLink>
+                </div>
             </nav>
         </div>
     );
