aa25d69d8720427af615740e2b77ffe85f289732
ValerXenus <valeraxenus@mail.ru>
2021-12-18 13:47:10 +0300

    Home controller + react upd

--- src/Controllers/HomeController.cs
+++ src/Controllers/HomeController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace KazanAirportWebApp.Controllers
 {
@@ -11,7 +7,6 @@ namespace KazanAirportWebApp.Controllers
         public ActionResult Index()
         {
             ViewBag.Title = "Home Page";
-
             return View();
         }
     }
