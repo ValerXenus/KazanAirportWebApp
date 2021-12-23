7f9cabb8349abb5c75b85833b1c1e5dea85ffcb5
ValerXenus <valeraxenus@mail.ru>
2021-12-23 22:20:27 +0300

    New get flights service

---
+++ FlightsMinerService/FlightsMinerInstaller.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+using System.ServiceProcess;
+
+namespace FlightsMinerService
+{
+    [RunInstaller(true)]
+    public partial class FlightsMinerInstaller : System.Configuration.Install.Installer
+    {
+        private ServiceInstaller _serviceInstaller;
+
+        private ServiceProcessInstaller _processInstaller;
+
+        public FlightsMinerInstaller()
+        {
+            InitializeComponent();
+            _serviceInstaller = new ServiceInstaller();
+            _processInstaller = new ServiceProcessInstaller();
+
+            _processInstaller.Account = ServiceAccount.LocalSystem;
+            _serviceInstaller.StartType = ServiceStartMode.Manual;
+            _serviceInstaller.ServiceName = "GetFlightsService";
+            Installers.Add(_processInstaller);
+            Installers.Add(_serviceInstaller);
+        }
+    }
+}
---
+++ FlightsMinerService/FlightsService.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Configuration;
+using System.IO;
+using System.ServiceProcess;
+using System.Threading;
+using Serilog;
+
+namespace FlightsMinerService
+{
+    public partial class FlightsService : ServiceBase
+    {
+        #region Private fields
+
+        /// <summary>
+        /// Интервал обновления списка авиарейсов
+        /// </summary>
+        private int _scheduleInterval;
+
+        /// <summary>
+        /// Фоновый процесс
+        /// </summary>
+        private Thread _processThread;
+
+        /// <summary>
+        /// Путь до файла с логом
+        /// </summary>
+        private string _logFilePath;
+
+        /// <summary>
+        /// Путь до файла со списком авиарейсов
+        /// </summary>
+        private string _flightsFilePath;
+
+        #endregion
+
+
+        public FlightsService()
+        {
+            InitializeComponent();
+
+            _scheduleInterval = Convert.ToInt32(ConfigurationManager.AppSettings["ThreadSleepTimeInMin"]);
+            _logFilePath = ConfigurationManager.AppSettings["LogFilePath"];
+            _flightsFilePath = ConfigurationManager.AppSettings["OutputFilePath"];
+        }
+
+        /// <summary>
+        /// Событие запуска сервиса
+        /// </summary>
+        /// <param name="args"></param>
+        protected override void OnStart(string[] args)
+        {
+            var logsFolder = Path.GetDirectoryName(_logFilePath);
+            if (!Directory.Exists(logsFolder) && !string.IsNullOrEmpty(logsFolder))
+                Directory.CreateDirectory(logsFolder);
+
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .WriteTo.File(_logFilePath, rollingInterval: RollingInterval.Day)
+                .CreateLogger();
+
+            try
+            {
+                Log.Information("Инициализация сервиса");
+
+                var start = new ThreadStart(getFlights);
+                _processThread = new Thread(start);
+                _processThread.Start();
+                
+            }
+            catch (Exception exception)
+            {
+                Log.Error(exception, "Произошла ошибка при инициализации потока обработки. Подробности: ");
+            }
+        }
+
+        /// <summary>
+        /// Событие остановки сервиса
+        /// </summary>
+        protected override void OnStop()
+        {
+            try
+            {
+                if (_processThread != null && _processThread.IsAlive)
+                    _processThread.Abort();
+
+                Log.Information("Завершение работы сервиса");
+                Log.CloseAndFlush();
+            }
+            catch (Exception exception)
+            {
+                Log.Error(exception, "Произошла ошибка при остановке сервиса. Подробности: ");
+            }
+        }
+
+        /// <summary>
+        /// Получение списка авиарейсов каждые _scheduleInterval минут
+        /// </summary>
+        private void getFlights()
+        {
+            while (true)
+            {
+                using (var writer = new StreamWriter(_flightsFilePath, true))
+                {
+                    writer.WriteLine(string.Format("Windows Service Called on " + DateTime.Now.ToString("dd/MM/yyyy hh:mm:ss tt")));
+                    writer.Close();
+                }
+                Thread.Sleep(_scheduleInterval * 60 * 1000);
+            }
+        }
+    }
+}
