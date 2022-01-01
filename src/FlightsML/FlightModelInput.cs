06db9a3ab2f61adc0e7a86ddfa219d3493a5e6b3
ValerXenus <valeraxenus@mail.ru>
2022-01-02 00:21:44 +0300

    Model fix

---
+++ src/FlightsML/FlightModelInput.cs
@@ -0,0 +1,10 @@
+﻿namespace FlightsML
+{
+    /// <summary>
+    /// Класс данных об авиарейсе для предсказания времени задержки
+    /// </summary>
+    public class FlightModelInput
+    {
+
+    }
+}
